16766269708c759d2d37fbb71362603afb356f50
Iacan Moreno da Costa Ramos <iacan.ramos@hotmail.com>
2021-06-01 23:43:36 -0300

    Add project files.

---
+++ GeekBurger.Products.Contract/ProductChanged.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeekBurger.Products.Contract
+{
+    public class ProductChanged
+    {
+        public ProductState State { get; set; }
+        public Product Product { get; set; }
+    }
+
+    public enum ProductState
+    {
+        Deleted = 2,
+        Modified = 3,
+        Added = 4
+    }
+}
